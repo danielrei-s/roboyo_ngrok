9c80c3b035c011edccb5b37d4b9617920f23e7ae
Daniel Reis <reis.maggot@gmail.com>
2023-03-16 11:55:47 +0000

    UI again

--- resources/views/layouts/sections/footer/footer.blade.php
+++ resources/views/layouts/sections/footer/footer.blade.php
@@ -1,19 +1,20 @@
 <!-- Footer-->
 <footer class="content-footer footer bg-footer-theme">
-  <div class="{{ (!empty($containerNav) ? $containerNav : 'container-fluid') }} d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
-    <div class="mb-2 mb-md-0">
+  <div class="{{ (!empty($containerNav) ? $containerNav : 'container-fluid') }} d-flex flex-wrap justify-content-between py-2 flex-row-reverse">
+    <div class="mb-2 mb-md-0 ">
       <div class="status-msg">
         @if (session()->has('success'))   {{--Puxar msg de registerbasic.php para manter utilizador informado --}}
           <div x-data="{show: true}"
-              x-init="setTimeout(() => show = false, 5000)"  
-              x-show="show">          {{--   Usa alpine.js para apanhar a mensagem anterior em 5segs. --}}
+          x-init="setTimeout(() => show = false, 5000)"  
+          x-show="show">          {{--   Usa alpine.js para apanhar a mensagem anterior em 5segs. --}}
             <p>{{ session('success') }}</p>
           </div>
         @endif
       </div>
-      Roboyo <script>
-        document.write(new Date().getFullYear())
-        </script>
+        Roboyo
+          <script>
+            document.write(new Date().getFullYear())
+          </script>
     </div>
   </div>
 </footer>
