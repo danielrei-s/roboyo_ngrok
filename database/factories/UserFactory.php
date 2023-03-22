9ab6b11b17af3465c309513bc6f59815fb39ec09
Daniel Reis <reis.maggot@gmail.com>
2023-03-22 17:13:52 +0000

    user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -47,4 +47,49 @@ class UserFactory extends Factory
             ];
         });
     }
+
+
+    public function admin()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+              'firstName' => 'Daniel',
+              'lastName' => 'Reis',
+              'sigla' => $this->faker->str_limit($this->faker->word(), 3, ''),
+              'email' => 'admin@admin.pt',
+              'email_verified_at' => now(),
+              'password' => bcrypt('admin'), // password
+              'remember_token' => Str::random(10),
+              'admin' => 1,
+              'manager' => 0,
+              'picture' => 'assets/img/avatars/5.png',
+              'contact' => '911222333',
+              'ativo' => '1',
+              'role' => 'Head of IT'
+            ];
+        });
+    }
+
+    /*php artisan tinker -> app/models/user::factory()->admin()->create()*/
+
+    public function manager()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+              'firstName' => 'Daniel',
+              'lastName' => 'Reis',
+              'sigla' => fake()->word(),
+              'email' => 'manager@manager.pt',
+              'email_verified_at' => now(),
+              'password' => bcrypt('manager'), // password
+              'remember_token' => Str::random(10),
+              'admin' => 0,
+              'manager' => 1,
+              'picture' => 'assets/img/avatars/5.png',
+              'contact' => '911222333',
+              'ativo' => '1',
+              'role' => 'Team Lead'
+            ];
+        });
+    }
 }
