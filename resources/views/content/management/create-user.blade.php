cbf001de5d5ccd186553d9f8b5f2c1f6547e894a
Daniel Reis <reis.maggot@gmail.com>
2023-04-03 19:05:11 +0100

    FE fixed to create user

--- resources/views/content/management/create-user.blade.php
+++ resources/views/content/management/create-user.blade.php
@@ -20,7 +20,7 @@
 
                         <div class="row">
                           <div class="col-md-4 mb-3 d-flex justify-content-center align-items-center flex-column">
-                            <div style="width: 150px; height: 150px; border-radius: 50%; overflow: hidden; margin-bottom: 10px;">
+                            <div style="width: 100px; height: 100px; border-radius: 50%; overflow: hidden; margin-bottom: 10px;">
                               <label for="photo" style="cursor: pointer;">
                                 <img src="assets/img/avatars/5.png" style="width: 100%; height: 100%; object-fit: cover;" alt="Profile picture">
                               </label>
@@ -44,16 +44,16 @@
                           <div class="row">
                             <div class="col-md-6 mb-3">
                                 <label for="firstname" class="form-label">First Name</label>
-                                <input type="text" class="form-control @error('firstname') is-invalid @enderror" id="firstname" name="firstname" placeholder="User's First Name" value="{{ old('firstname') }}" autofocus>
+                                <input type="text" class="form-control @error('firstname') is-invalid @enderror" id="firstname" name="firstname" placeholder=" First Name" value="{{ old('firstname') }}" autofocus>
                                 @error('firstname')
                                     <div class="invalid-feedback">{{ $message }}</div> {{-- feedback ao user sobre erros de input --}}
                                 @enderror
                                 </div>
-                              </div>
+
 
                               <div class="col-md-6 mb-3">
                                 <label for="lastname" class="form-label">Last Name</label>
-                                <input type="text" class="form-control @error('lastname') is-invalid @enderror" id="lastname" name="lastname" placeholder="User's Last Name" value="{{ old('lastname') }}" autofocus>
+                                <input type="text" class="form-control @error('lastname') is-invalid @enderror" id="lastname" name="lastname" placeholder=" Last Name" value="{{ old('lastname') }}" autofocus>
                                 @error('lastname')
                                     <div class="invalid-feedback">{{ $message }}</div> {{-- feedback ao user sobre erros de input --}}
                                 @enderror
@@ -61,7 +61,7 @@
 
                               <div class="col-md-6 mb-3">
                                 <label for="sigla" class="form-label">Sigla</label>
-                                <input type="text" class="form-control @error('sigla') is-invalid @enderror" id="sigla" name="sigla" placeholder="User's Sigla (3 letters)" value="{{ old('sigla') }}" autofocus>
+                                <input type="text" class="form-control @error('sigla') is-invalid @enderror" id="sigla" name="sigla" placeholder="Sigla (3 letters)" value="{{ old('sigla') }}" autofocus>
                                 @error('sigla')
                                     <div class="invalid-feedback">{{ $message }}</div> {{-- feedback ao user sobre erros de input --}}
                                     @enderror
@@ -69,34 +69,23 @@
 
                               <div class="col-md-6 mb-3">
                                 <label for="role" class="form-label">Role</label>
-                                <input type="text" class="form-control @error('role') is-invalid @enderror" id="role" name="role" placeholder="User's Role" value="{{ old('role') }}" autofocus>
+                                <input type="text" class="form-control @error('role') is-invalid @enderror" id="role" name="role" placeholder=" Role" value="{{ old('role') }}" autofocus>
                                 @error('role')
                                     <div class="invalid-feedback">{{ $message }}</div> {{-- feedback ao user sobre erros de input --}}
                                     @enderror
                                   </div>
 
-                              <div class="col-md-6 mb-3">
+                              <div class="col-md-12 mb-3">
                                 <label for="email" class="form-label">Email</label>
-                                <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="User's Email" value="{{ old('email') }}">
+                                <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder=" Email" value="{{ old('email') }}">
                                 @error('email')
                                 <div class="invalid-feedback">{{ $message }}</div> {{-- feedback ao user sobre erros de input --}}
                                 @enderror
                               </div>
-
-                              <div class="col-md-6 mb-3 form-password-toggle">
-                                <label class="form-label" for="password">Password</label>
-                                <div class="input-group input-group-merge">
-                                  <input type="password" id="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
-                                  <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-                                </div>
-                                @error('password')
-                                <div class="invalid-feedback">{{ $message }}</div> {{-- feedback ao user sobre erros de input --}}
-                                @enderror
+                              <div class="d-flex justify-content-end">
+                                <button type="button" class="btn btn-outline-secondary btn-md me-3" data-bs-dismiss="modal">Close</button>
+                                <button class="btn btn-primary">Sign up</button>
                               </div>
-                          </div>
-                          <div class="col-md-6 mb-3 mt-4 d-flex justify-content-end">
-                            <button type="button" class="btn btn-outline-secondary me-3" data-bs-dismiss="modal">Close</button>
-                            <button class="btn btn-primary">Sign up</button>
                           </div>
                         </div>
                     </form>
